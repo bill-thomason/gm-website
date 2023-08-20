b80a7b4c8fd5f46ef0d9013a26da85d95a508f35
Bill Thomason <thomason@adobe.com>
2023-08-19 21:10:18 -0600

    Marquee block work

---
+++ blocks/marquee/marquee.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+    
+    // loop through all divv containers.
+    for (let i = 0; i < block.children.length; i++) {
+        let child = block.children[i];
+        if ( i == 0 ) {
+            child.setAttribute("class", "background");
+        } else {
+            child.setAttribute("class", "foreground");
+        }
+    }
+}
